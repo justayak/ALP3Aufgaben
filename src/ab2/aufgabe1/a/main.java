ae4b69226678b5c2137d3e2d1467ed4d0e6a2314
Justayak <jutanke@googlemail.com>
2012-11-06 12:17:24 +0100

    init commit

---
+++ src/ab2/aufgabe1/a/main.java
@@ -0,0 +1,16 @@
+package ab2.aufgabe1.a;
+
+/**
+ * Created with IntelliJ IDEA.
+ * ALP III - Julian
+ * <p/>
+ * User: Baka
+ * Date: 06.11.12
+ * Time: 12:11
+ * To change this template use File | Settings | File Templates.
+ */
+public class main {
+    public static void main(String[] args){
+        System.out.println("hallo");
+    }
+}
