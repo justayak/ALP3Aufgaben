ca4bc8d2d408908aab64d99279a0126fa5f0561f
Justayak <jutanke@googlemail.com>
2012-11-29 14:13:13 +0100

    A6 (nicht nötig afaik)

---
+++ src/ab6/aufgabe1/Main.java
@@ -0,0 +1,13 @@
+package ab6.aufgabe1;
+
+/**
+ * Created with IntelliJ IDEA.
+ * ALP III - Julian
+ * <p/>
+ * User: Baka
+ * Date: 29.11.12
+ * Time: 14:02
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+}
